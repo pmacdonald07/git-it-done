804bdd2ba62611f112a866e8b959629ffb2c035b
Patrick M <patrickmacdonald07@gmail.com>
2022-08-31 18:22:35 -0400

    check headers for link and show user if there are more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningsEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
     console.log(repo);
@@ -12,6 +13,11 @@ var getRepoIssues = function (repo) {
                 .then(function(data) {
                     // pass response data to dom function
                     displayIssues(data);
+
+                    // check if api has paginated issues
+                    if (response.headers.get("Link")) {
+                        displayWarning(repo);
+                    }
                 });
             }
             else {
@@ -55,4 +61,17 @@ var displayIssues = function (issues) {
     };
 };
 
-getRepoIssues("pmacdonald07/work-day-scheduler");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningsEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningsEl.appendChild(linkEl);
+}
+
+getRepoIssues("facebook/react");
